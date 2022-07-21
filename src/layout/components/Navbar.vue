b346b950bb675581316c75b9f201e546baf17a65
Greg <402164923@qq.com>
2022-07-21 16:28:41 +0800

    头像加载失败BUG修复

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -10,7 +10,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="staffPhoto" class="user-avatar">
+          <img v-imagerror="defaultImg" :src="staffPhoto" class="user-avatar">
           <span class="name">{{ name }}</span>
           <i class="el-icon-caret-bottom" />
         </div>
@@ -43,6 +43,11 @@ export default {
     Hamburger
   },
   computed: {
+    data() {
+      return {
+        defaultImg: require('@/assets/common/head.jpg')
+      }
+    },
     ...mapGetters([
       'sidebar',
       'name',
