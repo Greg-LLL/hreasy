f419149f6ac5767a3d0a665df7439c3a17ab812c
Greg <402164923@qq.com>
2022-08-14 14:13:33 +0800

    给用户分配角色以及权限管理静态页面完成

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <!-- 靠右的按钮 -->
+      <page-tools>
+        <el-button slot="after" type="primary" size="small">添加权限</el-button>
+      </page-tools>
+      <!-- 表格 -->
+      <!-- 指定id为唯一属性的标识 -->
+      <el-table border :data="list" row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column align="center" label="标识" prop="code" />
+        <el-table-column label="描述" align="center" prop="description" />
+        <el-table-column label="操作" align="center">
+          <template v-slot="{row}">
+            <!-- 添加按钮只在访问权的层级显示 当type==1时 才显示添加按钮 -->
+            <el-button v-if="row.type ===1" type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils'
 export default {
-
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      // 将数据转化成了带children的树形结构
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+    }
+  }
 }
 </script>
 
